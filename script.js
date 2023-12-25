bd1ab36da0a26993698038d92a5e2b4374871c73
MarcosRinker <106207184+MarcosRinker@users.noreply.github.com>
2023-12-25 19:04:49 -0300

    Calendário pronto, javaScript implementado

---
+++ script.js
@@ -0,0 +1,103 @@
+const calendar = document.querySelector(".calendar"),
+date = document.querySelector(".date"),
+daysContainer = document.querySelector(".days"),
+prev = document.querySelector(".prev"),
+next = document.querySelector(".next");
+
+let today = new Date();
+let activeDay;
+let month = today.getMonth();
+let year = today.getFullYear();
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+
+//function to ad days
+
+function initCalendar(){
+//to get prev month days and current month all days and rem next month days
+
+    const firstDay = new Date(year, month, 1);
+    const lastDay = new Date(year, month + 1, 0);
+    const prevLastDay = new Date(year, month, 0);
+    const prevDays = prevLastDay.getDate();
+    const lastDate = lastDay.getDate();
+    const day = firstDay.getDay();
+    const nextDays = 7 - lastDay.getDay() - 1;
+
+    //update date top of calendar
+    date.innerHTML = months[month] + " " + year;   
+
+    //adding days on dom
+    let days = "";
+
+    //prev month days
+    for(let x = day; x > 0; x-- ){
+        days += `<div class = "day prev-date">${prevDays - x + 1}</div>`;
+    }
+
+    //current month days
+    for(let i = 1; i < lastDate; i++){
+        //if day is today add class today
+        if(
+            i == new Date().getDate() &&
+            year == new Date().getFullYear() && 
+            month == new Date().getMonth()){
+            days += `<div class = "day today">${i}</div>`;
+        }
+        //add remaing as it is
+        else{
+            days += `<div class = "day">${i}</div>`;
+        }
+    }
+
+    //next month days
+    for(let j = 1; j < nextDays; j++){
+        days += `<div class = "day next-date">${j}</div>`;
+    }
+
+    daysContainer.innerHTML = days;
+
+}
+
+initCalendar();
+
+//prev month
+function prevMonth(){
+    month--;
+    if(month < 0){
+        month = 11;
+        year--;
+    }
+    initCalendar();
+}
+
+//next month
+function nextMonth(){
+    month++;
+    if(month > 11){
+        month = 0;
+        year++;
+    }
+    initCalendar();
+}
+
+//add eventlistnner on prev and nex
+prev.addEventListener("click", prevMonth);
+next.addEventListener("click", nextMonth);
+
+//our calendar is ready
+//lets add goto date and goto today functionally
