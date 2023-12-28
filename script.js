a16d5aab34d5de429db5f0b094801ea8c5feac02
MarcosRinker <106207184+MarcosRinker@users.noreply.github.com>
2023-12-28 00:17:13 -0300

    Formatação de entrada de valores, ocultação menu add events

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const calendar = document.querySelector(".calendar"),
-date = document.querySelector(".date"),
-daysContainer = document.querySelector(".days"),
-prev = document.querySelector(".prev"),
-next = document.querySelector(".next"),
-todayBtn = document.querySelector(".today-btn"),
-gotoBtn = document.querySelector(".goto-btn"),
-dateInput = document.querySelector(".date-input");
+    date = document.querySelector(".date"),
+    daysContainer = document.querySelector(".days"),
+    prev = document.querySelector(".prev"),
+    next = document.querySelector(".next"),
+    todayBtn = document.querySelector(".today-btn"),
+    gotoBtn = document.querySelector(".goto-btn"),
+    dateInput = document.querySelector(".date-input"),
+    eventsContainer = document.querySelector(".events")   ;
 
 
 
@@ -30,11 +31,32 @@ const months = [
     "December",
 ];
 
+//default events array
+const eventsArr = [
+    {
+        day: 15,
+        month: 12,
+        year: 2023,
+        events: [
+            {
+                title: "Event 1",
+                time: "10:00 AM",
+            },
+            {
+                title: "Event 2",
+                time: "11:00 AM",
+            },
+        ],
+    },
+];
+
+
+
 
 //function to ad days
 
-function initCalendar(){
-//to get prev month days and current month all days and rem next month days
+function initCalendar() {
+    //to get prev month days and current month all days and rem next month days
 
     const firstDay = new Date(year, month, 1);
     const lastDay = new Date(year, month + 1, 0);
@@ -45,33 +67,57 @@ function initCalendar(){
     const nextDays = 7 - lastDay.getDay() - 1;
 
     //update date top of calendar
-    date.innerHTML = months[month] + " " + year;   
+    date.innerHTML = months[month] + " " + year;
 
     //adding days on dom
     let days = "";
 
     //prev month days
-    for(let x = day; x > 0; x-- ){
+    for (let x = day; x > 0; x--) {
         days += `<div class = "day prev-date">${prevDays - x + 1}</div>`;
     }
 
     //current month days
-    for(let i = 1; i < lastDate; i++){
+    for (let i = 1; i <= lastDate; i++) {
+        //check if event is present on that day
+        let event = false;
+        eventsArr.forEach((eventObj) => {
+          if (
+            eventObj.day === i &&
+            eventObj.month === month + 1 &&
+            eventObj.year === year
+          ) {
+            event = true;
+          }
+        });
+
+
+
         //if day is today add class today
-        if(
+        if (
             i == new Date().getDate() &&
-            year == new Date().getFullYear() && 
-            month == new Date().getMonth()){
-            days += `<div class = "day today">${i}</div>`;
+            year == new Date().getFullYear() &&
+            month == new Date().getMonth()
+        ) {
+            //if event found also add event class
+            if(event){
+                days += `<div class = "day today event">${i}</div>`;
+            }else{
+                days += `<div class = "day today">${i}</div>`;
+            }
         }
         //add remaing as it is
-        else{
-            days += `<div class = "day">${i}</div>`;
+        else {
+            if(event){
+                days += `<div class = "day  event">${i}</div>`;
+            }else{
+                days += `<div class = "day ">${i}</div>`;
+            }
         }
     }
 
     //next month days
-    for(let j = 1; j < nextDays; j++){
+    for (let j = 1; j < nextDays; j++) {
         days += `<div class = "day next-date">${j}</div>`;
     }
 
@@ -82,9 +128,9 @@ function initCalendar(){
 initCalendar();
 
 //prev month
-function prevMonth(){
+function prevMonth() {
     month--;
-    if(month < 0){
+    if (month < 0) {
         month = 11;
         year--;
     }
@@ -92,9 +138,9 @@ function prevMonth(){
 }
 
 //next month
-function nextMonth(){
+function nextMonth() {
     month++;
-    if(month > 11){
+    if (month > 11) {
         month = 0;
         year++;
     }
@@ -113,38 +159,38 @@ todayBtn.addEventListener("click", () => {
     month = today.getMonth();
     year = today.getFullYear();
     initCalendar();
-} );
+});
 
-dateInput.addEventListener("input" , (e) => {
+dateInput.addEventListener("input", (e) => {
     //allow only numbers remove anothing else
     dateInput.value = dateInput.value.replace(/[^0-9/]/g, "");
-    if(dateInput.value.length === 2){
+    if (dateInput.value.length === 2) {
         //add a slash if two numbers entered
         dateInput.value += "/";
     }
-    if(dateInput.value.length > 7){
+    if (dateInput.value.length > 7) {
         //dont allow more than / character
         dateInput.value = dateInput.value.slice(0, 7);
     }
     //if we remove untill slash its not removing
     //if backspace pressed
-    if(e.inputType === "deleteContentBackward"){
-        if(dateInput.value.length === 3){
+    if (e.inputType === "deleteContentBackward") {
+        if (dateInput.value.length === 3) {
             dateInput.value = dateInput.value.slice(0, 2);
         }
     }
 })
 
 
-gotoBtn.addEventListener("click" , gotoDate);
+gotoBtn.addEventListener("click", gotoDate);
 
 
 //function to go to entered date
-function gotoDate(){
+function gotoDate() {
     const dateArr = dateInput.value.split("/");
     //some date validation
-    if(dateArr.length === 2){
-        if(dateArr[0] > 0 && dateArr[0] < 13 && dateArr[1].length === 4){
+    if (dateArr.length === 2) {
+        if (dateArr[0] > 0 && dateArr[0] < 13 && dateArr[1].length === 4) {
             month = dateArr[0] - 1;
             year = dateArr[1];
             initCalendar();
@@ -157,3 +203,60 @@ function gotoDate(){
     alert("invalid date");
 
 }
+
+const addEventBtn = document.querySelector(".add-event"),
+    addEventContainer = document.querySelector(".add-event-wrapper"),
+    addEventCloseBtn = document.querySelector(".close"),
+    addEventTitle = document.querySelector(".event-name"),
+    addEventFrom = document.querySelector(".event-time-from"),
+    addEventTo = document.querySelector(".event-time-to");
+
+
+addEventBtn.addEventListener("click", () => {
+    addEventContainer.classList.toggle("active");
+});
+addEventCloseBtn.addEventListener("click", () => {
+    addEventContainer.classList.remove("active");
+});
+
+
+document.addEventListener("click", (e) => {
+    //if click outside
+    if (e.target != addEventBtn && !addEventContainer.contains(e.target)) {
+        addEventContainer.classList.remove("active");
+    }
+});
+
+
+//allow 50 chars in title
+addEventTitle.addEventListener("input", (e) => {
+    addEventTitle.value = addEventTitle.value.slice(0, 50);
+})
+
+//time format in from and to time
+addEventFrom.addEventListener("input", (e) => {
+    //remove anything else numbers
+    addEventFrom.value = addEventFrom.value.replace(/[^0-9:]/g, "");
+    //if two numbers entered auto add :
+    if (addEventFrom.value.length == 2) {
+        addEventFrom.value += ":";
+    }
+    //dont let user enter more than 5 chars
+    if (addEventFrom.value.length > 5) {
+        addEventFrom.value = addEventFrom.value.slice(0, 5);
+    }
+})
+
+//same with to time
+addEventTo.addEventListener("input", (e) => {
+    //remove anything else numbers
+    addEventTo.value = addEventTo.value.replace(/[^0-9:]/g, "");
+    //if two numbers entered auto add :
+    if (addEventTo.value.length == 2) {
+        addEventTo.value += ":";
+    }
+    //dont let user enter more than 5 chars
+    if (addEventTo.value.length > 5) {
+        addEventTo.value = addEventTo.value.slice(0, 5);
+    }
+})
